cfab8cb84a7c9284aa8f637deb20d59f77a46c06
LwRuan <lws.ruan@gmail.com>
2021-07-31 21:43:35 +0800

    hello window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
 
 int main(){
-    std::cout << "hello world" << std::endl;
-    return 0;
+    // Init GLFW
+    if(!glfwInit()){
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL 3.2
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window;
+    window = glfwCreateWindow( 1024, 768, "OpenGLRender", NULL, NULL);
+    if( window == NULL ){
+        std::cerr << "Failed to open GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    // Init GL3W
+    if (gl3wInit()) {
+            fprintf(stderr, "failed to initialize OpenGL\n");
+            return -1;
+    }
+    if (!gl3wIsSupported(3, 2)) {
+            fprintf(stderr, "OpenGL 3.2 not supported\n");
+            return -1;
+    }
+    printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION),
+            glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do{
+        glClear( GL_COLOR_BUFFER_BIT );
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+        glfwWindowShouldClose(window) == 0 );
 }
